f5ff467dd799344342d80f19c3dace427b5e17fb
Arvindh <echoesincharade@gmail.com>
2023-01-17 19:03:08 +0530

    Data binding

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Dish } from '../shared/dish';
 
 @Component({
   selector: 'app-dishdetail',
@@ -7,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DishdetailComponent implements OnInit {
 
+  @Input()
+  dish: Dish;
   constructor() { }
 
   ngOnInit() {
